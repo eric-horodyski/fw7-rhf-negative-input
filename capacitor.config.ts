02a163aa8c1ff442be1f019d5507bf096068ee98
Eric Horodyski <horodyski@ionic.io>
2023-09-25 10:13:43 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fw7-rhf-negative-input',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
